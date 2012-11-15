0726c14d1d5955c4f4fc3abf985f9e862730226b
octopus <octopus@nyc.rr.com>
2012-11-14 22:44:04 -0500

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 SampleApp4Rspec::Application.routes.draw do
+
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
